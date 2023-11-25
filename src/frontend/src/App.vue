9162bb0713e727a9e0ee82cc105fc15ee6b3e86f
CatherineLISHUANG <catherinelishuang1997@gmail.com>
2023-11-25 23:33:22 +0100

    add popup windows for Detail buttons to present patient info

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -125,6 +125,8 @@ export default {
     <div class="row align-items-start">
       <div class="patient-rack col">
         <!-- WIP: add patient -->
+        <br />
+        <br />
         <div>
           <button
             type="button"
@@ -208,9 +210,51 @@ export default {
               <div>{{ item.content }}</div>
             </div>
             <div class="col">
-              <button type="button" class="btn btn-outline-primary">
+              <!-- Button trigger modal -->
+              <button
+                type="button"
+                class="btn btn-outline-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#staticBackdrop"
+              >
                 Details
               </button>
+              <!-- Modal -->
+              <div
+                class="modal fade"
+                id="staticBackdrop"
+                data-bs-backdrop="static"
+                data-bs-keyboard="false"
+                tabindex="-1"
+                aria-labelledby="staticBackdropLabel"
+                aria-hidden="true"
+              >
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="staticBackdropLabel">
+                        Modal title
+                      </h5>
+                      <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div class="modal-body">Patient Info</div>
+                    <div class="modal-footer">
+                      <button
+                        type="button"
+                        class="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
