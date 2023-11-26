50b53a5c85513c148fc6ab78a79170a9d1fdad0d
jagger-en <japman2400@gmail.com>
2023-11-26 04:16:42 +0100

    Remove warnings

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -71,14 +71,13 @@ export default {
                     fetch(`http://127.0.0.1:8000/api/v1/maintenance?nested=true`).then((response) => {
                         response.json().then((data) => {
                             if (response.ok) {
-                                console.log(data)
                                 const maintenancesFromDatabase = data.map(d => {
                                   return {
                                       start: new Date(d.start_time),
                                       end: new Date(d.end_time),
                                       title: d.machine.machine_name,
                                       content: `${d.machine.machine_name}`,
-                                      class: "maintenance",
+                                      class: "closed",
                                       appointment: d,
                                       split: this.determineSplit(d.machine.id), // Has to match the id of the split you have set (or integers if none).
                                     }
@@ -445,7 +444,6 @@ export default {
         </div>
 
         <vue-cal
-          :special-hours="specialHours"
           hide-weekends
           :disable-views="['years', 'year']"
           :min-cell-width="minCellWidth"
